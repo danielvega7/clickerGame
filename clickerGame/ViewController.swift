326f71f0a98a5b54e3311377b503f6d7531d73e1
Daniel Vega <daniel.vega23@d155.org>
2022-03-15 09:50:49 -0500

    started on clicker game

--- clickerGame/ViewController.swift
+++ clickerGame/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
+    let db = Firestore.firestore()
+    
+    var ref = Database.database().reference()
+
+    var infoListener: ListenerRegistration!
+    
+    var current = 0
+    var highScore = [Int]()
+    
+    @IBOutlet weak var timeLabelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func startButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickMeButtonAction(_ sender: UIButton) {
+    }
+    
+    func read() {
+        
+    }
+    func write() {
+        db.collection("names").document("movieArray").setData(["movie": highScore], merge: true)
+    }
 }
 
